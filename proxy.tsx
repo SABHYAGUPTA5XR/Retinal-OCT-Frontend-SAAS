379e7371b070f5169f0712a8c357921b744b0ec4
Sabhya Gupta <sabhyagupta4xr@gmail.com>
2025-11-02 01:57:00 +0530

    corrected proxy

--- proxy.tsx
+++ proxy.tsx
@@ -1,19 +1,16 @@
-// This file should now be named "proxy.ts"
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-import { clerkMiddleware } from '@clerk/nextjs/server';
+const isProtectedRoute = createRouteMatcher(['/product(.*)'])
 
-export default clerkMiddleware({
-  // These routes will be accessible to everyone (no login required)
-  publicRoutes: ["/", "/about"],
-
-  // These routes will be completely ignored by Clerk's authentication
-  ignoredRoutes: ["/api/webhook", "/api/getproducts", "/api/stripewebhook"],
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
-  // This matcher is correct
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
